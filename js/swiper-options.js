8b042a9748d96963be24fece21c68075f6b84479
Miroslavetsh <dfgshte@gmail.com>
2021-03-07 11:46:14 +0200

    Added promo with preview

---
+++ js/swiper-options.js
@@ -0,0 +1,14 @@
+import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js'
+
+const swiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.swiper__pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper__button--next',
+        prevEl: '.swiper__button--prev',
+    },
+    spaceBetween: 10,
+    autoHeight: true,
+})
